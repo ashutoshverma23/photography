0a5ed65fe458e1da2a6a368e758d31cb9003700a
Ashutosh Verma <ashutoshrgnict@gmail.com>
2024-01-03 16:28:32 +0530

    images updated with pagination
    Required images are added and i created 2 additional pages to store my other photos

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,10 +1,1 @@
-//Adding javascript components in it
-document.getElementById('zoom-btn').addEventListener('click', function() {
-    const photo = document.getElementById('photo');
-     
-    if (photo.style.transform === 'scale(2)') {
-       photo.style.transform = '';
-    } else {
-       photo.style.transform = 'scale(2)';
-    }
-   });
+
